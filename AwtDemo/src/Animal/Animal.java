ef0260bd4d40eb4c5a5f4f1c56f79b152cf80d6e
hanupkumar <hanupkumar.talasila@outlook.com>
2019-02-14 16:08:03 +0530

    push animal

---
+++ AwtDemo/src/Animal/Animal.java
@@ -0,0 +1,16 @@
+package Animal;
+
+public class Animal {
+public void makeNoise() {
+	System.out.println("every animal makes noise......!");
+}
+public void eat() {
+	System.out.println("every animal eats...!");
+}
+public void roam() {
+	System.out.println("every animal roams....!");
+}
+public void sleep() {
+	System.out.println("every animal sleeps....!");
+}
+}
